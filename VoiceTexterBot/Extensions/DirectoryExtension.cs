262d68fd268fb165c8b860d62fe2e386d7a28b70
olegnizh <olegnizh@mail.ru>
2024-03-23 19:19:14 +0300

    Extensions

---
+++ VoiceTexterBot/Extensions/DirectoryExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceTexterBot.Extensions
+{
+    public static class DirectoryExtension
+    {
+        public static string GetSolutionRoot()
+        {
+            var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var fullname = Directory.GetParent(dir).FullName;
+            var projectRoot = fullname.Substring(0, fullname.Length - 4);
+            return Directory.GetParent(projectRoot)?.FullName;
+        }
+    }
+
+}
---
+++ VoiceTexterBot/Extensions/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceTexterBot.Extensions
+{
+    public static class StringExtension
+    {
+        public static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+
+}
